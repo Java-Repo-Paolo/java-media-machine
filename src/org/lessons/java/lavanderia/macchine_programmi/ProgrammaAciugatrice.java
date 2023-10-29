1cebb38a84160a0a2549e278d01b7207c083c2cd
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-29 13:57:18 +0100

    Commit 9 - Lavanderia - Mod ProgrammaAsciugatrice

--- src/org/lessons/java/lavanderia/macchine_programmi/ProgrammaAciugatrice.java
+++
@@ -1,7 +0,0 @@
-package org.lessons.java.lavanderia.macchine_programmi;
-
-public class ProgrammaAciugatrice extends Programma{
-    public ProgrammaAciugatrice(int numero, String nome, int durata, int gettoni) {
-        super(numero, nome, durata, gettoni);
-    }
-}
