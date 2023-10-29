51b67894fd602b59dba59f00fa65ce3077d09857
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-29 15:29:00 +0100

    Commit 14 - Lavanderia - mod Class Main

--- src/org/lessons/java/lavanderia/Main.java
+++ src/org/lessons/java/lavanderia/Main.java
@@ -1,10 +1,23 @@
 package org.lessons.java.lavanderia;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Lavanderia lavanderia = new Lavanderia(3, 2);
-        lavanderia.stampaTabella();
+
+        String comando = "";
+
+        Scanner scanner = new Scanner(System.in);
+        while (!comando.equals("esci")){
+            lavanderia.stampaTabella();
+            System.out.println(lavanderia.getLastMessage());
+            lavanderia.stampaComandi();
+            System.out.println("> ");
+            comando = scanner.nextLine();
+        }
+        scanner.close();
     }
 }
