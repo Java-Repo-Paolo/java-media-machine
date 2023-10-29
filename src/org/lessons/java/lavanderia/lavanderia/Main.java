b7223a78fe9c2066aa3cd6cc77656b5ae78cecd6
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-29 14:31:08 +0100

    Commit 11 - Lavanderia - add Class Main

---
+++ src/org/lessons/java/lavanderia/lavanderia/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.lavanderia.lavanderia;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lavanderia lavanderia = new Lavanderia(3, 2);
+        System.out.println(lavanderia);
+    }
+}
